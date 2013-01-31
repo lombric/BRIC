26a3949f1285601ccb99cde33fed18a80998be36
tiago-ramos <antonio.ramos@cpnv.ch>
2013-01-31 08:26:36 +0100

    database1.1

--- database/Lombric.sql
+++ database/Lombric.sql
@@ -1,33 +1,52 @@
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+-- phpMyAdmin SQL Dump
+-- version 3.5.1
+-- http://www.phpmyadmin.net
+--
+-- Client: localhost
+-- Généré le: Jeu 31 Janvier 2013 à 08:25
+-- Version du serveur: 5.5.25
+-- Version de PHP: 5.4.4
 
-CREATE SCHEMA IF NOT EXISTS `lombric` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci ;
-USE `lombric` ;
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
--- -----------------------------------------------------
--- Table `lombric`.`group`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `lombric`.`group` (
-  `idgroup` INT NOT NULL AUTO_INCREMENT ,
-  `name` VARCHAR(45) NOT NULL ,
-  `description` TEXT NULL ,
-  `specifications` TEXT NULL ,
-  `date_begin` DATE NULL ,
-  `date_end` DATE NULL ,
-  `fk_idgroup` INT NULL DEFAULT NULL ,
-  PRIMARY KEY (`idgroup`) ,
-  INDEX `fk_group_group_idx` (`fk_idgroup` ASC) ,
-  CONSTRAINT `fk_group_group`
-    FOREIGN KEY (`fk_idgroup` )
-    REFERENCES `lombric`.`group` (`idgroup` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+--
+-- Base de données: `lombric`
+--
 
-USE `lombric` ;
+-- --------------------------------------------------------
 
+--
+-- Structure de la table `group`
+--
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+CREATE TABLE `group` (
+  `idgroup` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `description` text,
+  `specifications` text,
+  `date_begin` date DEFAULT NULL,
+  `date_end` date DEFAULT NULL,
+  `fk_idgroup` int(11) DEFAULT NULL,
+  `hide` tinyint(4) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`idgroup`),
+  KEY `fk_group_group_idx` (`fk_idgroup`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Contenu de la table `group`
+--
+
+INSERT INTO `group` (`idgroup`, `name`, `description`, `specifications`, `date_begin`, `date_end`, `fk_idgroup`, `hide`) VALUES
+(3, 'test', '', '', '2013-01-30', '2013-01-30', NULL, 0),
+(4, '12', '', '', '0000-00-00', '0000-00-00', NULL, 1);
+
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `group`
+--
+ALTER TABLE `group`
+  ADD CONSTRAINT `fk_group_group` FOREIGN KEY (`fk_idgroup`) REFERENCES `group` (`idgroup`) ON DELETE NO ACTION ON UPDATE NO ACTION;
