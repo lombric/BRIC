282d77b92cb86366e400d85cddcd09e98fb65dd1
Damien Rochat <rochat.damien@gmail.com>
2013-03-20 13:58:05 +0100

    Add GIIX support

--- app/protected/config/main-local.php
+++ app/protected/config/main-local.php
@@ -1,18 +1,20 @@
 <?php
 
-// http://www.yiiframework.com/wiki/155/the-directory-structure-of-the-yii-project-site
-
 return array(
-	// application components
-	'components'=>array(
-		'db' => array(
-			'connectionString' => 'mysql:host=localhost;dbname=lombric', /*localhost connexion with the lombric.sql script*/
-			'emulatePrepare' => true,
-			'username' => 'lombric',
-			'password' => 'lombric',
-			'charset' => 'utf8',
-		)
-	)
+    'components'=>array(
+        'db' => array(
+            'connectionString' => 'mysql:host=localhost;dbname=lombric',
+            'emulatePrepare' => true,
+            'username' => 'root',
+            'password' => 'dsa321',
+            'charset' => 'utf8',
+        ),
+    ),
+    'modules'=> array(
+        'gii'=>array(
+            'password'=>'dsa321',
+        ),
+    ),
 );
 
 ?>
---
+++ app/protected/models/MembersGroups.php
@@ -0,0 +1,10 @@
+<?php
+
+Yii::import('application.models._base.BaseMembersGroups');
+
+class MembersGroups extends BaseMembersGroups
+{
+	public static function model($className=__CLASS__) {
+		return parent::model($className);
+	}
+}
