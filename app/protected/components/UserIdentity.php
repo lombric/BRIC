b1a4989b0b464148e290c064a2f261a0488e7360
Palumbo Timothée <timothee.palumbo@cpnv.ch>
2013-03-20 15:35:49 +0100

    Add login with the database

--- app/protected/components/UserIdentity.php
+++ app/protected/components/UserIdentity.php
@@ -17,17 +17,24 @@ class UserIdentity extends CUserIdentity
 	 */
 	public function authenticate()
 	{
-		$users=array(
-			// username => password
-			'demo'=>'demo',
-			'admin'=>'admin',
-		);
-		if(!isset($users[$this->username]))
-			$this->errorCode=self::ERROR_USERNAME_INVALID;
-		else if($users[$this->username]!==$this->password)
-			$this->errorCode=self::ERROR_PASSWORD_INVALID;
-		else
-			$this->errorCode=self::ERROR_NONE;
-		return !$this->errorCode;
-	}
+		$username = $this->username;
+        $password = $this->password;
+
+        $criteria = new CDbCriteria;
+		$criteria->compare("username",$username, true, "AND");
+		$criteria->compare("password",$password, true);
+
+        $user = Members::model()->find($criteria);
+
+	    if($user === NULL){
+                $this->errorCode=self::ERROR_USERNAME_INVALID;
+            	$this->errorCode=self::ERROR_PASSWORD_INVALID;
+        }
+        else{
+                $this->username = $user->username;
+                $this->errorCode=self::ERROR_NONE;
+
+        }
+        return !$this->errorCode;
+}
 }
